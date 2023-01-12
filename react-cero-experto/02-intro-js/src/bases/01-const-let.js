4159317d2471bdc68c97b597669744a528aa0dce
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro-2.local>
2023-01-11 20:28:51 -0600

    02-intro-js

---
+++ react-cero-experto/02-intro-js/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// const: su valor no cambia
+const nombre = 'alvaro';
+const apellido = 'larsen';
+
+// let: su valor si cambia
+let valorDado = 5;
+valorDado = 2;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    const nombre = 'robert'
+    let valorDado = 6;
+    console.log(nombre, valorDado);
+}
+
+console.log(nombre, apellido, valorDado);
